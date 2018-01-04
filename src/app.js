5092b884a90c265f0aba60e30b8f9bcb1cbac935
Carlos Huerta <carlos.huertaso@udlap.mx>
2018-01-03 22:22:56 -0600

    Quitado firebase temporalmente de la app

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import 'react-dates/lib/css/_datepicker.css'; // Importaciones de estilo del cal
 import "react-dates/initialize"; // Importacion nueva necesaria para que sirva el calendario
 import AppRouter from './routers/AppRouter';
 import configuraAlmacen from './almacen/configuraAlmacen'; // Importacion del Almacen REDUX y su modelo con reducers
-import './firebase/firebase'; // Importaciones de Firebase para que corra
+//import './firebase/firebase'; // Importaciones de Firebase para que corra //Por Ahora no usaremos Firebase
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css'; // Imports de BS
 
 // acceso al modelo del almacen con REDUX
